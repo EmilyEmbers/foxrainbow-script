45972b56107a3e7a37680af40304291ef8bec835
Feedback Fox <34089784+FeedbackFox@users.noreply.github.com>
2019-02-28 16:26:35 +0100

    Update rgbrainbow.js

--- rgbrainbow.js
+++ rgbrainbow.js
@@ -1,57 +1,58 @@
 (function(){
-  let textspeed = 1;
-  let backgroundspeed = -1;
+    let textspeed = 1;
+    let backgroundspeed = 1;
 
-  var textcounter = 0;
-  var rainbowClass = document.getElementsByClassName('rainbow');
-  var lengtharray = []; //Remembers the length of the content with class=rainbow so for loop doesnt change color of undefined
-  var rainbowarray = []; //Becomes the final array containing all the <span>'d text
-  var finalarray = []; //Becomes the final array referencing the spanned text and changing the color
-  var a = 0;
+    let classestoberainbowed = document.getElementsByClassName('foxrainbow');
+    let backgroundtoberainbowed = document.getElementsByClassName('foxrainbowbg');
+    let charstoberainbowed = [];
+    let spanstoberainbowed = [];
+    
 
-  for(var i = 0; i < rainbowClass.length; i++)
-  {
-    var tempvar = rainbowClass[i].innerText.split("");
-    lengtharray[i] = tempvar.length;
-    rainbowarray.push(rainbowClass[i].innerText.split(""));
-    console.log(rainbowarray[i]);
-  }
+    for(let i = 0; i < classestoberainbowed.length; i++) {
+        charstoberainbowed.push(classestoberainbowed[i].innerText.split(""));
+        classestoberainbowed[i].innerHTML = charstoberainbowed[i].map(function(char) {
+            return '<span>' + char + "</span>";
+        }).join('');
 
-  for(var i = 0; i < rainbowarray.length; i++)
-  {
-    rainbowClass[i].innerHTML = rainbowarray[i].map(function(char) {
-      return '<span>' + char + '</span>';
-    }).join('');
-    finalarray[i] = rainbowClass[i].children;
-    console.log(finalarray[i]);
-  }
+    }
 
-  for(var i = 0; i < rainbowClass.length; i++)
-  {
-    rainbowcolors(i, finalarray[i].length)
-  }
+    for(let i = 0; i < classestoberainbowed.length; i++) {
+        let temphtmlcollection = [].slice.call(classestoberainbowed[i].children)
+        for(let j = 0; j < temphtmlcollection.length; j++) {
+            spanstoberainbowed.push(temphtmlcollection[j]);
+        }
+    }
 
-  function rainbowcolors(a,b)
-  {
-    setInterval(function colorchange() {
-      for (var i = 0; i < b; i++)
-      {
-        finalarray[a][i].style.color = 'hsl(' + (textcounter + Math.floor(i * textspeed)) + ', 100%, 70%';
-      }
-      textcounter++;
-    }, 7);
-  }
+    textcolorchange(spanstoberainbowed, textspeed);
 
-  var backgroundcounter = 0;
-  var rainbowId = document.getElementsByClassName('rainbowbackground');
+    let backgroundcounter = 0;
+    setInterval(() => {
 
-
-  setInterval(function colorchange() {
-	   for(var i = 0; i < rainbowId.length; i++)
-	    {
-	      rainbowId[i].style.backgroundColor = 'hsl(' + (backgroundcounter + Math.floor(i * backgroundspeed)) + ', 100%, 70%';
-	    }
-    backgroundcounter++;
-  }, 15);
+        for(let i = 0; i < backgroundtoberainbowed.length; i++) {
+            backgroundtoberainbowed[i].style.backgroundColor = 'hsl(' + (backgroundcounter + Math.floor(i * backgroundspeed)) + ', 100%, 70%';
+        }
+        backgroundcounter++;
+    }, 15);
 
 })()
+
+function textcolorchange(rainbowarray, rainbowspeed) {
+    let counterarray = [];
+
+    for(let i = 0; i < rainbowarray.length; i++) {
+        counterarray[i] = 0 + i;
+    }
+    setInterval(() => {
+
+        for(let i = 0; i < rainbowarray.length; i++) {
+            rainbowarray[i].style.color = 'hsl(' + (counterarray[i] + Math.floor(i * rainbowspeed)) + ', 100%, 70%';
+            if(counterarray[i] == 360)
+            {
+                counterarray[i] = 0;
+            }
+            else {
+                counterarray[i]++;
+            }
+        }
+    }, 7);
+}
